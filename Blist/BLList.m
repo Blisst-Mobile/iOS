5e517c58708b1a2bc7b076a95c4e8624bf3e3a82
Andrew Breckenridge <asbreckenridge@me.com>
2014-05-24 17:20:56 -0400

    init

---
+++ Blist/BLList.m
@@ -0,0 +1,13 @@
+//
+//  BLList.m
+//  Blist
+//
+//  Created by Andrew Breckenridge on 5/24/14.
+//  Copyright (c) 2014 Andrew Breckenridge. All rights reserved.
+//
+
+#import "BLList.h"
+
+@implementation BLList
+
+@end
